a3f5da21a62844bfad4d4b0002269a50d40bd739
BreeWooden <woodebri000@hsestudents.org>
2025-11-25 13:10:19 -0500

    Add files via upload

---
+++ shopping.js
@@ -0,0 +1,74 @@
+console.log("🍋", "🫑", "🍞", "🥩", "🥛", "🥣", "🦞", "🥪");
+
+// grab the add button element
+const addBtn = document.querySelector("#add-btn");
+
+//this func will add items to the users cart
+const updateList = (event) => {
+    const icon = event.target;
+    icon.classList.toggle("bi-bag");
+    icon.classList.toggle("bi-bag-check");
+    icon.classList.toggle("in-cart");
+}; // end of updateList
+
+//func will remove the div
+const removeItem = (event) => {
+    const icon = event.target;
+    icon.parentElement.remove();
+}; //end of removeItem
+
+//add event listener
+addBtn.addEventListener("click", () => {
+    // get info from user
+    const itemInput = document.querySelector("#item");
+    const emojiSelect = document.querySelector("#department");
+
+    const item = itemInput.value.trim();
+    const emoji = emojiSelect.value;
+
+    if(item){
+
+        //now create div
+        const itemDiv = buildItem(emoji, item);
+        //now add the div to the page
+        document.querySelector(".right").append(itemDiv);
+    } else {
+        alert("Please enter a name for the item.");
+    }
+
+    //user features
+    itemInput.value = "";
+    itemInput.focus();
+    emojiSelect.value = "🥣";
+
+}); //end of event listener
+
+
+
+// this func will build the html for each item
+function buildItem(emoji, item){
+    //create html elements
+    const itemDiv = document.createElement("div");
+    const emojiSpan = document.createElement("span");
+    const itemSpan = document.createElement("span");
+    const bagIcon = document.createElement("i");
+    const trashIcon = document.createElement("i");
+
+    //add the text and class info
+    itemDiv.className = "item";
+    emojiSpan.className = "emoji";
+    emojiSpan.innerText = emoji;
+    itemSpan.className = "item-description";
+    itemSpan.innerText = item;
+    bagIcon.className = "bi bi-bag add-icon";
+    trashIcon.className = "bi bi-trash delete-icon";
+    //icon event listeners
+    bagIcon.addEventListener("click", updateList);
+    trashIcon.addEventListener("click", removeItem);
+
+    //build the component
+    itemDiv.append(emojiSpan, itemSpan, bagIcon, trashIcon);
+
+    return itemDiv;
+
+} //end of build item func
